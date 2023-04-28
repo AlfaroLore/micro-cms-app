891a4c96e4f57c3cbeef056cc97b01d961474d2a
nalfaro1 <nalfaro@tetradefense.com>
2023-04-27 21:51:13 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1 className="text-3xl font-bold text-blue-600">Vite + React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
